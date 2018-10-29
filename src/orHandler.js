90aacd2dca184f5d53cd25f48f14167bae041b0a
Jorge Zapata Guridi <jorge.guridi@hulu.com>
2018-10-28 23:48:35 -0700

    And, or and xor handlers

--- src/orHandler.js
+++ src/orHandler.js
@@ -1,15 +1,15 @@
 import { BitwiseCalculator } from './calc';
 
-export const AndHandler = {
+export const OrHandler = {
     canHandle(handlerInput) {
-        return handlerInput.requestEnvelope.request.type === 'IntentRequest' && handlerInput.requestEnvelope.request.intent.name === 'andIntent';
+        return handlerInput.requestEnvelope.request.type === 'IntentRequest' && handlerInput.requestEnvelope.request.intent.name === 'orIntent';
     },
     handle(handlerInput) {
         const { requestEnvelope, responseBuilder } = handlerInput;
         const { intent } = requestEnvelope.request;
         const number1 = intent.slots.integer_one.value;
         const number2 = intent.slots.integer_two.value;
-        const ans = BitwiseCalculator.and(number1, number2);
+        const ans = BitwiseCalculator.or(number1, number2);
         let text = 'Sorry, there was an error with the request. Please try again.';
         if (ans !== null) {
             text = `The answer is ${ans}`;
